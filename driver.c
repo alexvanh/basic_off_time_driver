21d9ba62ef329a08c567c045478ab91297d2359b
alexvanh <alexvanheuvelen@gmail.com>
2015-01-08 17:59:54 -0500

    publish driver

---
+++ driver.c
@@ -0,0 +1,116 @@
+/*
+ * "Off Time Basic Driver" for ATtiny controlled flashlights
+ *  Copyright (C) 2014 Alex van Heuvelen (alexvanh)
+ *
+ * Basic firmware demonstrating a method for using off-time to
+ * switch modes on attiny13 drivers such as the nanjg drivers (a feature
+ * not supported by the original firmware).
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+ 
+ 
+ 
+/* This firmware uses off-time memory for mode switching without
+ * hardware modifications on nanjg drivers. This is accomplished by
+ * storing a flag in an area of memory that does not get initialized.
+ * There is enough energy stored in the decoupling capacitor to
+ * power the SRAM and keep data during power off for about 500ms.
+ *
+ * When the firmware starts a byte flag is checked. If the flashlight
+ * was off for less than ~500ms all bits will still be 0. If the
+ * flashlight was off longer than that some of the bits in SRAM will
+ * have decayed to 1. After the flag is checked it is reset to 0.
+ * Being off for less than ~500ms means the user half-pressed the
+ * switch (using it as a momentary button) and intended to switch modes.
+ *
+ * This can be used to store any value in memory. However it is not
+ * guaranteed that all of the bits will decay to 1. Checking that no
+ * bits in the flag have decayed acts as a checksum and seems to be
+ * enough to be reasonably certain other SRAM data is still valid.
+ *
+ * In order for this to work brown out detection must be enabled by
+ * setting the correct fuse bits. I'm not sure why this is, maybe
+ * reduced current consumption due to the reset being held once the
+ * capacitor voltage drops below the threshold?
+ */
+
+#include <avr/io.h>
+#include <stdlib.h>
+
+
+#define F_CPU 4800000
+
+// PWM configuration
+#define PWM_PIN PB1
+#define PWM_LVL OCR0B
+#define PWM_TCR 0x21
+#define PWM_SCL 0x01
+
+
+// store in uninitialized memory so it will not be overwritten and
+// can still be read at startup after short (<500ms) power off
+uint8_t noinit_decay __attribute__ ((section (".noinit")));
+uint8_t noinit_mode __attribute__ ((section (".noinit")));
+
+
+int main(void)
+{
+    // it seems data from before power off must be copied
+    // ASAP after start up
+    uint8_t mode = noinit_mode;
+    uint8_t decay = noinit_decay; // true if memory has decayed
+
+
+    // PWM setup 
+    // set PWM pin to output
+    DDRB |= _BV(PWM_PIN);
+    // PORTB = 0x00; // initialised to 0 anyway
+
+    // Initialise PWM on output pin and set level to zero
+    TCCR0A = PWM_TCR;
+    TCCR0B = PWM_SCL;
+
+    PWM_LVL = 0;
+
+    // memory has decayed or mode needs to loop back around
+    // (or the mode is invalid)
+    if (decay || mode > 3) // there are 4 modes
+    {
+        mode = 0;
+    }
+
+    // set noinit data for next boot
+    noinit_decay = 0;
+    noinit_mode = mode + 1;
+
+    switch(mode){
+        case 0:
+        PWM_LVL = 0xFF;
+        break;
+        case 1:
+        PWM_LVL = 0x40;
+        break;
+        case 2:
+        PWM_LVL = 0x10;
+        break;
+        case 3:
+        PWM_LVL = 0x04;
+        break;
+    }
+
+    while(1);
+    return 0;
+}
